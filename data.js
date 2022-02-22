62f7f3821f1e9647244077c18b4a72d0cf0d98ab
int2itive <int2itive@yahoo.co.uk>
2022-02-22 13:13:09 +0000

    initial commit

---
+++ data.js
@@ -0,0 +1,128 @@
+const data = [
+	{
+	    "schedule": [
+	        {
+	            "status": "Headline",
+	            "artist": {
+		        	"name": "Cezanne Poetess",
+		        	"performs": "Spoken Word/Poetry",
+		        	"orig": "Jam",
+		        	"img": "",
+		        	"flag": ""
+		        }
+	            "time": "20:20",
+	            "duration": "30",
+		    },
+	        {
+	            "status": "Intro",
+	            "artist": {
+		        	"name": "Lady Esi",
+		        	"performs": "Introduction",
+		        	"orig": "Gha",
+		        	"img": "",
+		        	"flag": ""
+		        }
+	            "time": "18:35",
+	            "duration": "5",
+		    },
+	        {
+	            "status": "Guest",
+	            "artist": {
+		        	"name": "Sola Story",
+		        	"performs": "Storytelling",
+		        	"orig": "Yoruba",
+		        	"img": "",
+		        	"flag": ""
+		        }
+	            "time": "18:40",
+	            "duration": "20",
+		    },
+	        {
+	            "status": "Guest",
+	            "artist": {
+		        	"name": "Evelyn Chronicle",
+		        	"performs": "Storytelling",
+		        	"orig": "Jam",
+		        	"img": "",
+		        	"flag": ""
+		        }
+	            "time": "19:00",
+	            "duration": "10",
+		    },
+	        {
+	            "status": "Guest",
+	            "artist": {
+		        	"name": "Oshon Li",
+		        	"performs": "Storytelling",
+		        	"orig": "Jam",
+		        	"img": "",
+		        	"flag": ""
+		        }
+	            "time": "19:15",
+	            "duration": "10",
+		    },
+	        {
+	            "status": "Guest",
+	            "artist": {
+		        	"name": "Rare Diamond",
+		        	"performs": "Storytelling",
+		        	"orig": "Jam",
+		        	"img": "",
+		        	"flag": ""
+		        }
+	            "time": "19:25",
+	            "duration": "10",
+		    },
+	        {
+	            "status": "Guest",
+	            "artist": {
+		        	"name": "Lady Esi",
+		        	"performs": "Storytelling",
+		        	"orig": "Jam",
+		        	"img": "",
+		        	"flag": ""
+		        }
+	            "time": "19:35",
+	            "duration": "10",
+		    },
+	        {
+	            "status": "Guest",
+	            "artist": {
+		        	"name": "Sista J Africa",
+		        	"performs": "Storytelling",
+		        	"orig": "Jam",
+		        	"img": "",
+		        	"flag": ""
+		        }
+	            "time": "20:05",
+	            "duration": "10",
+		    }
+		]
+	}
+]
+
+/**
+ * Thursday, 24th February - Rebirth Event Schedule
+ * 
+ * 18:15 - Music and Drumming
+ * 
+ * 18:35 - Introductions, Lady Esi        5mins
+ * 
+ * 19:00 - Evelyn Chronicle               15mins
+ * 
+ * 19:15 - Oshon Li                       10mins
+ * 
+ * 19:25 - Rare Diamond                   10mins
+ * 
+ * 19:35 - Lady Esi                       10mins
+ * 
+ * 19:45 - BREAK                          15mins
+ * 
+ * 20:05 - Sista J Africa                 15mins
+ * 
+ * 20:20 - Cezanne Poetess                30mins
+ * 
+ * So, effetively you have the artists ('cause Sola counts as an artist)
+ * organizing the event because there isn't a robust operations protocol
+ * in place.
+ */
